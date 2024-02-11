3bfa68c1a1316beafe46e3151722e673fe89e573
Valerii Petryniak <valerii15298@gmail.com>
2024-02-11 23:22:29 +0200

    frontend lot: show images using base64 strings as src

--- web/src/LotImages.tsx
+++ web/src/LotImages.tsx
@@ -1,4 +1,3 @@
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
@@ -8,18 +7,15 @@ import {
 } from "@/components/ui/carousel";
 
 export function LotImages({ images }: { images: string[] }) {
+  if (!images.length) {
+    return null;
+  }
   return (
     <Carousel className="w-full max-w-xs">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
-            </div>
+        {images.map((src, idx) => (
+          <CarouselItem key={idx}>
+            <img src={src} />
           </CarouselItem>
         ))}
       </CarouselContent>

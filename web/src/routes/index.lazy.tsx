95ffc2aa32d51ec8925f067cab9909000e7d9d57
Valerii Petryniak <valerii15298@gmail.com>
2024-02-11 21:17:14 +0200

    refine routes

--- web/src/routes/index.lazy.tsx
+++ web/src/routes/index.lazy.tsx
@@ -1,15 +1,30 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
-import { App } from "../App";
+import { SignedIn } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink, loggerLink } from "@trpc/client";
+import { useState } from "react";
+import { trpc } from "../trpc";
+import { Lots } from "../Lots";
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 });
 
 function Index() {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [loggerLink(), httpBatchLink({ url: "/trpc" })],
+    })
+  );
+
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-      <App />
-    </div>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <SignedIn>
+          <Lots />
+        </SignedIn>
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 }

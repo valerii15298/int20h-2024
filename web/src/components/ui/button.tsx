931fc8ee238fee18ab61b5b6bae3d0dfc9af7568
Valerii Petryniak <valerii15298@gmail.com>
2024-02-11 22:57:47 +0200

    shadcn/ui add default type='button' for the button

--- web/src/components/ui/button.tsx
+++ web/src/components/ui/button.tsx
@@ -46,6 +46,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        type="button"
         {...props}
       />
     )

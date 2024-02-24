ce4e8e176e9be8376c5a9adfd5a5e731bb3d3f52
Valerii Petryniak <valerii15298@gmail.com>
2024-02-24 04:47:54 +0200

    fix db:push for drizzle-orm

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "drizzle-kit";
-import { env } from "./src/config.js";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
 
+const { parsed: env } = expand(config({ processEnv: {}, path: "../.env" }));
 export default {
   schema: "./src/schema.ts",
   driver: "pg",
   dbCredentials: {
-    connectionString: env.POSTGRES_URL,
+    connectionString: env!["POSTGRES_URL"]!,
   },
 } satisfies Config;

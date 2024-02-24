f8bd5138a0fda5c3c7d3470e0848e7364f4216eb
Valerii Petryniak <valerii15298@gmail.com>
2024-02-24 03:00:27 +0200

    group cdn related stuff in cloudinary.ts

--- api/src/cloudinary.ts
+++ api/src/cloudinary.ts
@@ -1,5 +1,6 @@
 import { v2 } from "cloudinary";
 import { env } from "./config.js";
+import { Request, Response } from "express";
 
 export const cloudinary = v2;
 
@@ -8,3 +9,18 @@ cloudinary.config({
   api_key: env.CLOUDINARY_API_KEY,
   api_secret: env.CLOUDINARY_API_SECRET,
 });
+
+export function uploadMiddleware(req: Request, res: Response) {
+  const fileStream = cloudinary.uploader.upload_stream(
+    {},
+    function (err, image) {
+      if (err) {
+        res.status(500).send(JSON.stringify(err));
+        return;
+      }
+      if (!image) return;
+      res.send(image.url);
+    }
+  );
+  req.pipe(fileStream);
+}

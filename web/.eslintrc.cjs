29be90cb8fe9f6707f139ef5421d0d0a7117c29b
Valerii Petryniak <valerii15298@gmail.com>
2024-02-25 23:04:42 +0200

    make eslint config stricter for react-fast-refresh

--- web/.eslintrc.cjs
+++ web/.eslintrc.cjs
@@ -6,14 +6,10 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist"],
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
-    ],
     "@typescript-eslint/no-unused-vars": [
       "warn",
       { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },

a32698606b1712ee15d3cc5c7ab3d4b4e7401e2c
Valerii Petryniak <valerii15298@gmail.com>
2024-02-26 06:42:31 +0200

    api: add typescrip-eslint plugin parser options

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,28 +1,28 @@
 // @ts-check
 
-import * as tsEslint from "typescript-eslint";
 // @ts-expect-error eslint does not have types yet
 import eslint from "@eslint/js";
 // @ts-expect-error eslint-config-prettier does not have correct types yet
 import eslintConfigPrettier from "eslint-config-prettier";
+// @ts-expect-error tsEslint does not have correct types yet
+import tsEslint from "typescript-eslint";
 
 export default tsEslint.config(
   {
-    // TODO fix and add these files to eslint
     ignores: ["dist"],
   },
 
-  // {
-  //     plugins: {
-  //         '@typescript-eslint': tseslint.plugin,
-  //     },
-  //     languageOptions: {
-  //         parser: tseslint.parser,
-  //         parserOptions: {
-  //             project: true,
-  //         },
-  //     },
-  // },
+  {
+    languageOptions: {
+      parser: tsEslint.parser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsEslint.plugin,
+    },
+  },
 
   eslint.configs.all,
   // ...tsEslint.configs.all,

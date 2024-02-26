5a52891bff0880dd6522c09c0ee828a57d6012b2
Valerii Petryniak <valerii15298@gmail.com>
2024-02-26 02:40:52 +0200

    web: prettier: format code

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -18,7 +18,7 @@ const TanStackRouterDevtools =
           default: res.TanStackRouterDevtools,
           // For Embedded Mode
           // default: res.TanStackRouterDevtoolsPanel
-        }))
+        })),
       );
 
 export const Route = createRootRoute({
